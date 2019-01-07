2b8088a1a3865398b6260b79d521bebc17cc3f81
parmezan38 <luka38.stojanac@gmail.com>
2019-01-07 18:24:53 +0100

    added background-attachment: fixed to body

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <div id="app">
       <h1>Chord-O</h1>
-      <h2>An app to help you with practicing chords</h2>
+      <h2>A simple app to help you with practicing chords</h2>
       <Chord/>
     </div>
   </div>
@@ -36,5 +36,6 @@ html {
 }
 body {
   background-image: linear-gradient(15deg, #EDB4A1, #A1E0ED );
+  background-attachment:fixed;
 }
 </style>
